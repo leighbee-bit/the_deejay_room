5fbfd79dbf19c87c21efbdae3038e2b9d194e65c
leigh <leighlamar.dantes@bison.howard.edu>
2026-03-27 12:43:55 -0400

    integration

---
+++ ios-app/The Deejay Room/Color+Hex.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Hex.swift
+//  The Deejay Room
+//
+//  Created by Leigh D on 3/24/26.
+//
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: .alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let r = Double((int >> 16) & 0xFF) / 255
+        let g = Double((int >> 8) & 0xFF) / 255
+        let b = Double(int & 0xFF) / 255
+        self.init(red: r, green: g, blue: b)
+    }
+}
